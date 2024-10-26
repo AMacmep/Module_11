7428ada808d68808e1be720919c83514c5ea4438
AMacmep <cherkashinandrey@mail.ru>
2024-10-26 09:34:57 +0700

    1

--- module_11_1.py
+++ module_11_1.py
@@ -1,6 +1,21 @@
 # Домашнее задание по теме "Обзор сторонних библиотек Python"
 import requests
+import numpy as np
+import pandas as pd
+import pillow
 
-r=requests.put('https://httpbin.org/put',data={'key':'value'})
-print(r)
+
+url_name='https://github.com/AMacmep/Module_11'
+head_url=requests.head(url_name)
+print(head_url)
+
+
+
+
+# xlsx = pd.ExcelFile("Excel file 1.xlsx")
+# df = pd.read_excel(xlsx, "Sheet1")
+# print(df)
+# df.sort_index(axis=1, ascending=False)
+# print(df)
+
 
